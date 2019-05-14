0593adb320c25dac576eb80c54663f453861557e
Justine Lee <justinelee103@gmail.com>
2019-05-14 17:19:56 -0400

    rename and heroku test

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Namchi Kimchi</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -19,7 +19,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
+          <Link to="/login">LoginTEST!</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
       )}
